cf495da7868d7194e6033191d1a995c874db46e9
sabas1080 <sabasjimenez@gmail.com>
2021-05-15 23:16:01 -0500

    update asf4_config

--- ports/atmel-samd/asf4_conf/saml21/peripheral_clk_config.h
+++ ports/atmel-samd/asf4_conf/saml21/peripheral_clk_config.h
@@ -348,6 +348,38 @@
 #define CONF_GCLK_TC0_FREQUENCY 48000000
 #endif
 
+// <y> TC Clock Source
+// <id> tc_gclk_selection
+
+// <GCLK_PCHCTRL_GEN_GCLK0_Val"> Generic clock generator 0
+
+// <GCLK_PCHCTRL_GEN_GCLK1_Val"> Generic clock generator 1
+
+// <GCLK_PCHCTRL_GEN_GCLK2_Val"> Generic clock generator 2
+
+// <GCLK_PCHCTRL_GEN_GCLK3_Val"> Generic clock generator 3
+
+// <GCLK_PCHCTRL_GEN_GCLK4_Val"> Generic clock generator 4
+
+// <GCLK_PCHCTRL_GEN_GCLK5_Val"> Generic clock generator 5
+
+// <GCLK_PCHCTRL_GEN_GCLK6_Val"> Generic clock generator 6
+
+// <GCLK_PCHCTRL_GEN_GCLK7_Val"> Generic clock generator 7
+
+// <i> Select the clock source for TC.
+#ifndef CONF_GCLK_TC1_SRC
+#define CONF_GCLK_TC1_SRC GCLK_PCHCTRL_GEN_GCLK0_Val
+#endif
+
+/**
+ * \def CONF_GCLK_TC1_FREQUENCY
+ * \brief TC1's Clock frequency
+ */
+#ifndef CONF_GCLK_TC1_FREQUENCY
+#define CONF_GCLK_TC1_FREQUENCY 48000000
+#endif
+
 // <y> DAC Clock Source
 
 // <GCLK_PCHCTRL_GEN_GCLK0_Val"> Generic clock generator 0
