a1f22711e67e52a98b911d27b5d1d903ff2dc130
Eduardo <werowero.edy@gmail.com>
2021-06-07 13:52:54 -0500

    update boards

--- ports/atmel-samd/boards/bast_saml21/mpconfigboard.h
+++ ports/atmel-samd/boards/bast_saml21/mpconfigboard.h
@@ -8,16 +8,6 @@
 // No microcontroller.nvm
 #define CIRCUITPY_INTERNAL_NVM_SIZE 0
 
-#define DEFAULT_I2C_BUS_SCL (&pin_PA08)
-#define DEFAULT_I2C_BUS_SDA (&pin_PA09)
-
-#define DEFAULT_SPI_BUS_SCK (&pin_PA17)
-#define DEFAULT_SPI_BUS_MOSI (&pin_PA16)
-#define DEFAULT_SPI_BUS_MISO (&pin_PA19)
-
-#define DEFAULT_UART_BUS_RX (&pin_PA01)
-#define DEFAULT_UART_BUS_TX (&pin_PA00)
-
 #define IGNORE_PIN_PA03     1
 #define IGNORE_PIN_PA12     1
 #define IGNORE_PIN_PA13     1
