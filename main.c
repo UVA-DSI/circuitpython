296b04d884c51c9886be20688e699ae50aee25c5
Eduardo <werowero.edy@gmail.com>
2021-06-07 13:56:51 -0500

    add led flashing

--- main.c
+++ main.c
@@ -557,6 +557,12 @@ STATIC void __attribute__ ((noinline)) run_boot_py(safe_mode_t safe_mode) {
 #endif
 }
 
+void delay(unsigned long milis){
+	for(unsigned long i=0;i<milis;i++){
+        __asm("NOP");
+    }
+}
+
 STATIC int run_repl(void) {
     int exit_code = PYEXEC_FORCED_EXIT;
     stack_resize();
@@ -588,11 +594,30 @@ int __attribute__((used)) main(void) {
     init_status_leds();
     rgb_led_status_init();
 
+    //port regs = 0x40002800
+    int *pt;
+    //PAC
+    //pt = (int *)0x44000000;
+    //*pt =0x00FF000AU;
+    //Port stuff
+	pt = (int *)0x40002800;
+    *pt =1<<23;
+    pt = (int *)0x40002810;
+    //pt =1<23;
+    for(int i=0;i<5;i++){
+        *pt=1<<23;
+        delay(500000);
+        *pt=0x00000000;
+        delay(500000);
+    }
+
     // Wait briefly to give a reset window where we'll enter safe mode after the reset.
     if (safe_mode == NO_SAFE_MODE) {
         safe_mode = wait_for_safe_mode_reset();
     }
+    
 
+    
     stack_init();
 
     // Create a new filesystem only if we're not in a safe mode.
